d7a881d5efa94a110c74ad94e7071c31606407e8
simenfhe <simenfhe@stud.ntnu.no>
2023-05-10 14:31:01 +0200

    idk

--- src/components/Vcardlist.jsx
+++ src/components/Vcardlist.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect} from 'react';
+import axios from 'axios';
 
 
 function Contactlist(){
@@ -19,7 +20,7 @@ function Contactlist(){
             }
         }
 
-
+        getlist()
 
     },[])
 
