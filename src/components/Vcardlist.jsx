5461dc7626b6032e38ce19d5895d95df9470113a
simenfhe <simenfhe@stud.ntnu.no>
2023-05-15 11:52:38 +0200

    up/download
    Co-authored-by: Marianne Mytting <mariaamy@stud.ntnu.no>
    Co-authored-by: ThomasSelvig <ThomasSelvig@users.noreply.github.com>

--- src/components/Vcardlist.jsx
+++ src/components/Vcardlist.jsx
@@ -1,26 +1,16 @@
 import React, { useState, useEffect} from 'react';
 import axios from 'axios';
 
-function Contactlist({backgroundColor = "lightblue"}){
+function Contactlist({backgroundColor = "lightblue", border = "1px black solid"}){
 
-    const [contacts, setContacts] = useState()
-    const [input, setInput] = useState({
-        address: '',
-        birthday: '',
-        firstname: '',
-        name: '',
-        organization: '',
-        email: '',
-        telephone: ''
-        
-    })
-
+    const [contacts, setContacts] = useState() 
+    
     useEffect(() =>{
 
         async function getlist(){
             console.log("halla")
             try{
-                const response = await axios.get('https://cloud-cache.onrender.com/contacts')
+                const response = await axios.get('https://cloud-cache.onrender.com/contacts/pass/password')
                 console.log(response)
                 setContacts(response.data)
             }catch(err){
@@ -32,30 +22,13 @@ function Contactlist({backgroundColor = "lightblue"}){
 
     },[])
 
-    const handleInputChange = (event) => {
-        const {name, value} = event.target
-        setInput(prevState => ({ ...prevState, [name] : value}))
-    }
-
-    const handleFormSubmit = async (event) => {
-        event.preventDefault()
-
-        try{
-            await axios.post('https://cloud-api-vcard-production-34ea.up.railway.app/',{
-                address: input.address,
-                birthday: input.birthday,
-                firstname: input.firstname,
-                name: input.name,
-                organization: input.organization,
-                email: input.email,
-                telephone: input.telephone
-            })
-        }catch(err){console.error(err)}
-    }
-
     const styles = {
         backgroundColor,
         fontWeight: "bold"
+    }
+
+    const td = {
+      border: "2px black solid"
     }
 
     return (
@@ -77,13 +50,13 @@ function Contactlist({backgroundColor = "lightblue"}){
                         <tbody>
                             {contacts.map((contact,i)=>
                                     <tr key={i} className='tr'>
-                                        <td>{contact.address}</td>
-                                        <td>{contact.birthday}</td>
-                                        <td>{contact.firstname}</td>
-                                        <td>{contact.name}</td>
-                                        <td>{contact.organization}</td>
-                                        <td>{contact.email}</td>
-                                        <td>{contact.telephone}</td>
+                                        <td style={td}>{contact.address}</td>
+                                        <td style={td}>{contact.birthday}</td>
+                                        <td style={td}>{contact.firstname}</td>
+                                        <td style={td}>{contact.name}</td>
+                                        <td style={td}>{contact.organization}</td>
+                                        <td style={td}>{contact.email}</td>
+                                        <td style={td}>{contact.telephone}</td>
                                     </tr>
                             )}
                         </tbody>
@@ -91,29 +64,12 @@ function Contactlist({backgroundColor = "lightblue"}){
                 )
                 : <p>no matches</p>
             }
-            <form onSubmit={handleFormSubmit}>
-                <input type="hidden" name="version" value={"3.0"}/>
-                <label htmlFor="address">address</label>
-                <input type="text" name="address" value={input.address} onChange={handleInputChange}/>
-                <label htmlFor="birthday">birthday</label>
-                <input type="text" name="birthday" value={input.birthday} onChange={handleInputChange}/>
-                <label htmlFor="firstname">firstname</label>
-                <input type="text" name="firstname" value={input.firstname} onChange={handleInputChange}/>
-                <label htmlFor="name">name</label>
-                <input type="text" name="name" value={input.name} onChange={handleInputChange}/>
-                <label htmlFor="organization">organization</label>
-                <input type="text" name="organization" value={input.organization} onChange={handleInputChange}/>
-                <label htmlFor="email">email</label>
-                <input type="text" name="email" value={input.email} onChange={handleInputChange}/>
-                <label htmlFor="telephone">telephone</label>
-                <input type="number" name="telephone" value={input.telephone} onChange={handleInputChange}/>
-                <button type='submit'>submit</button>
-
-            </form>
+            <div>
+                <a href="https://cloud-api-vcard-production-34ea.up.railway.app/upload/pass/password" id='left'>Upload</a>
+                <a href="https://cloud-api-vcard-production-34ea.up.railway.app/contacts/vcard/pass/password">Download</a>
+            </div>
         </>
     )
-
 }
-
 
 export default Contactlist
